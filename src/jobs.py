434ad7122662c2892ae635e144aff0ff509cbdcf
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 11:16:38 -0300

    feat: :sparkles: funcao read jobs finalizada

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,9 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        return list(reader)
