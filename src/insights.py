66f1212f0156c9a731798dd2326eeb8085f4169b
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 11:31:36 -0300

    feat: :sparkles: funcao filter by job type finalizada

--- src/insights.py
+++ src/insights.py
@@ -4,27 +4,13 @@ from src.jobs import read
 def get_unique_job_types(path):
     content = read(path)
     job_types = set()
-    for row in content:
-        job_types.add(row["job_type"])
+    for job in content:
+        job_types.add(job["job_type"])
     return job_types
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return [job for job in jobs if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):
