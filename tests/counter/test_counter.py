dccd8021fcbc1dd492395c01852b2c47d30e274e
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 13:22:15 -0300

    test: :sparkles: elaboracao teste funcao test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    file_path = "src/jobs.csv"
+    languages = ["Python", "Javascript"]
+    expected_results = [1639, 122]
+
+    for language in range(len(languages)):
+        assert (
+            count_ocurrences(file_path, languages[language])
+            == expected_results[language]
+        )
