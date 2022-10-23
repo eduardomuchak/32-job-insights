887eee04f316aa4fa70f9492c641efa4ae2e6f2a
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 14:48:32 -0300

    test: :sparkles: implementa teste funcao test sort by criteria
    usando a funcao get min salary da pasta insights

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,11 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
+from src.insights import get_min_salary
 
 
 def test_sort_by_criteria():
-    pass
+    path = "src/jobs.csv"
+    jobs = read(path)
+
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == str(get_min_salary(path))
