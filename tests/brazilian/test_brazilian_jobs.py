b96a31e57a496c9bb2197bc114510bda5e779e10
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 14:27:14 -0300

    test: :sparkles: elaboracao teste funcao test brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    file = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in file:
+        assert "type" in job
+        assert "title" in job
+        assert "salary" in job
