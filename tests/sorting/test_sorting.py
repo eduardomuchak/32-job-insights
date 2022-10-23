4fbb917fb419a4fdd612816b33aa634d23751665
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 14:51:01 -0300

    test: :sparkles: adiciona funcao get max salary na funcao test sort by criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,6 +1,6 @@
 from src.sorting import sort_by
 from src.jobs import read
-from src.insights import get_min_salary
+from src.insights import get_min_salary, get_max_salary
 
 
 def test_sort_by_criteria():
@@ -9,3 +9,6 @@ def test_sort_by_criteria():
 
     sort_by(jobs, "min_salary")
     assert jobs[0]["min_salary"] == str(get_min_salary(path))
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == str(get_max_salary(path))
