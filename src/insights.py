026c9b8544893b1bee901437508fc7a1bbd2d6cb
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-23 11:34:06 -0300

    feat: :sparkles: funcao get unique industries finalizada

--- src/insights.py
+++ src/insights.py
@@ -14,21 +14,11 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    content = read(path)
+    industries = set()
+    for job in content:
+        industries.add(job["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):
