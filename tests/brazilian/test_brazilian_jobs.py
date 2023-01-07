127bc737acf7d77c4eee15cb4ac2835cd92e869f
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-07 10:16:28 -0300

    refactor: :fire: remove arquivos trybe

--- tests/brazilian/test_brazilian_jobs.py
+++
@@ -1,9 +0,0 @@
-from src.brazilian_jobs import read_brazilian_file
-
-
-def test_brazilian_jobs():
-    file = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
-    for job in file:
-        assert "type" in job
-        assert "title" in job
-        assert "salary" in job
